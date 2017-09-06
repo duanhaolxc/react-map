4d691d2de21d016328fddd993a40c6d200c17d60
duanhaolxc <duanhaolxc@126.com>
2017-09-06 15:35:37 +0800

    Update AMapPolyline.kt

--- android/src/main/java/cn/qiuxiang/react/amap3d/maps/AMapPolyline.kt
+++ android/src/main/java/cn/qiuxiang/react/amap3d/maps/AMapPolyline.kt
@@ -64,13 +64,36 @@ class AMapPolyline(context: Context) : ReactViewGroup(context) {
         this.coordinates = ArrayList((0 until coordinates.size())
                 .map { coordinates.getMap(it) }
                 .map {
-                    LatLongData(it.getDouble("latitude"), it.getDouble("longitude"), if (it.hasKey("speer")) it.getDouble("speer") else 0.0)
+                    LatLongData(it.getDouble("latitude"), it.getDouble("longitude"), if (it.hasKey("speer")) it.getDouble("speer") else 3.0)
                 })
+        fixPolyLines()
+    }
+
+    fun setColors(colors: ReadableArray) {
+        this.colors = ArrayList((0..colors.size() - 1).map { colors.getInt(it) })
+        fixPolyLines()
+    }
+
+    fun addToMap(map: AMap) {
+        polyline = map.addPolyline(polylineRunningOptions!!
+                .color(color)
+                .colorValues(PolyLineColors)
+                .width(width)
+                .useGradient(gradient)
+                .geodesic(geodesic)
+                .setDottedLine(dashed)
+                .zIndex(zIndex))
+        MapTools().getLatLngBounds(polylineRunningOptions!!.points, map)
+
+    }
+
+    private fun fixPolyLines() {
+        if (this.coordinates.size>0)
         for (i in this.coordinates.indices) {
             if (i == 0) {//拆分坐标
-                builder.append("LINESTRING(" + this.coordinates[i].lattitude + " " + this.coordinates[i].longitude)
+                builder.append("LINESTRING(" + this.coordinates[i].lattitude + " " + this.coordinates[i].longitude + " " + this.coordinates[i].speer)
             } else {
-                builder.append("," + this.coordinates[i].lattitude + " " + this.coordinates[i].longitude)
+                builder.append("," + this.coordinates[i].lattitude + " " + this.coordinates[i].longitude + " " + this.coordinates[i].speer)
             }
 
             if (i == this.coordinates.size - 1) {
@@ -82,56 +105,29 @@ class AMapPolyline(context: Context) : ReactViewGroup(context) {
         d.compress(d.points!![0], d.points!![d.points!!.size - 1])
         for (i in d.points!!.indices) {
             if (d.points!![i].index > -1) {
-
                 if (polylineRunningOptions == null) {
                     polylineRunningOptions = PolylineOptions()
                 }
                 polylineRunningOptions!!.add(LatLng(d.points!![i].x, d.points!![i].y))
+                var speer = d.points!![i].speer
+                if (this.colors.size > 0) {
+                    if (0 < speer && speer < 2)
+                        PolyLineColors.add(PolyLineColors.size, this.colors[0])
+                    else if (2 < speer && speer < 4) {
+                        PolyLineColors.add(PolyLineColors.size, this.colors[1])
+                    } else if (4 < speer && speer < 6) {
+                        PolyLineColors.add(PolyLineColors.size, this.colors[2])
+                    } else if (6 < speer && speer < 7) {
+                        PolyLineColors.add(PolyLineColors.size, this.colors[3])
+                    } else if (7 < speer && speer < 10) {
+                        PolyLineColors.add(PolyLineColors.size, this.colors[4])
+                    } else {
+                        PolyLineColors.add(PolyLineColors.size, this.colors[5])
+                    }
+                }
             }
         }
         polyline?.options = polylineRunningOptions
         builder.setLength(0)
-
-    }
-
-    fun setColors(colors: ReadableArray) {
-        this.colors = ArrayList((0..colors.size() - 1).map { colors.getInt(it) })
-    }
-    fun polyLine() {
-            polylineRunningOptions = PolylineOptions()
-            for (i in this.coordinates.indices) {
-                var speer = this.coordinates[i].speer
-                polyLines.add(LatLng(this.coordinates[i].lattitude, this.coordinates[i].longitude))
-                if (0 < speer && speer < 2)
-                    PolyLineColors.add(PolyLineColors.size, this.colors[0])
-                else if (2 < speer && speer < 4) {
-                    PolyLineColors.add(PolyLineColors.size,this.colors[1])
-                } else if (4 < speer && speer < 6) {
-                    PolyLineColors.add(PolyLineColors.size, this.colors[2])
-                } else if (6 < speer && speer < 7) {
-                    PolyLineColors.add(PolyLineColors.size,this.colors[3])
-                } else if (7 < speer && speer < 10) {
-                    PolyLineColors.add(PolyLineColors.size,this.colors[4])
-                } else {
-                    PolyLineColors.add(PolyLineColors.size,this.colors[5])
-                }
-            }
-            polylineRunningOptions!!.addAll(PathSmoothTool().pathOptimize(polyLines))
-    }
-
-    fun addToMap(map: AMap) {
-        if (this.colors.size>0){
-            polyLine()
-        }
-        polyline = map.addPolyline(polylineRunningOptions!!
-                .color(color)
-                .colorValues(PolyLineColors)
-                .width(width)
-                .useGradient(gradient)
-                .geodesic(geodesic)
-                .setDottedLine(dashed)
-                .zIndex(zIndex))
-        MapTools().getLatLngBounds(polylineRunningOptions!!.points,map)
-
     }
 }
