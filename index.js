fb82ac25bfd77a49243f45712ef0eee18ed4536d
duanhaolxc <duanhaolxc@126.com>
2017-12-25 11:46:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import Marker from './components/maps/Marker'
 import Polyline from './components/maps/Polyline'
 import Polygon from './components/maps/Polygon'
 import Circle from './components/maps/Circle'
+import HeatTitle from './components/maps/HeatTitle'
 import Navigation from './components/navigation'
 import MapUtils from './components/Utils'
 
@@ -10,12 +11,14 @@ MapView.Marker = Marker
 MapView.Polyline = Polyline
 MapView.Polygon = Polygon
 MapView.Circle = Circle
+MapView.HeatTitle = HeatTitle
 
 export default MapView
 export {
   MapView,
   Marker,
   Polyline,
+  HeatTitle,
   Polygon,
   Circle,
   Navigation,
