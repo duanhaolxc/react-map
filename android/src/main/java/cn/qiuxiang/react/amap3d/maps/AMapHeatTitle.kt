e2d8646f6fbe7a8cb3aaa99ac9efef66901bb4ec
duanhaolxc <duanhaolxc@126.com>
2018-01-16 15:25:17 +0800

    Update AMapHeatTitle.kt

--- android/src/main/java/cn/qiuxiang/react/amap3d/maps/AMapHeatTitle.kt
+++ android/src/main/java/cn/qiuxiang/react/amap3d/maps/AMapHeatTitle.kt
@@ -1,7 +1,6 @@
 package cn.qiuxiang.react.amap3d.maps
 
 import android.content.Context
-import android.util.Log
 import com.amap.api.maps.AMap
 import com.amap.api.maps.model.*
 import com.facebook.react.bridge.ReadableArray
@@ -16,17 +15,22 @@ class AMapHeatTitle(context: Context) : ReactViewGroup(context) {
     private var coordinates: Collection<WeightedLatLng> = ArrayList()
 
     fun setCoordinates(coordinates: ReadableArray) {
-        this.coordinates = ArrayList((0 until coordinates.size())
-                .map { coordinates.getMap(it) }
-                .map { WeightedLatLng(LatLng(it.getDouble("latitude"), it.getDouble("longitude")), it.getDouble("weight")) })
-        Log.e("AMapHeatTitle", this.coordinates.toString())
+        try {
+            this.coordinates = ArrayList((0 until coordinates.size())
+                    .map { coordinates.getMap(it) }
+                    .map { WeightedLatLng(LatLng(it.getDouble("latitude"), it.getDouble("longitude")), it.getDouble("weight")) })
+
+        }catch (e:Exception){
+            e.printStackTrace()
+        }
+
     }
 
     fun addToMap(map: AMap) {
         // 构建热力图 HeatmapTileProvider
         val builder = HeatmapTileProvider.Builder()
         builder.weightedData(coordinates)
-         builder.transparency(0.9)
+        builder.transparency(0.9)
         val heatmapTileProvider = builder.build()
         val tileOverlayOptions = TileOverlayOptions()
         tileOverlayOptions.tileProvider(heatmapTileProvider) // 设置瓦片图层的提供者
